2fd721df5abb464488025f98e9ead4f2bb39d126
hmattosaffinitiv <hmattos@affinitiv.com>
2023-06-21 10:37:12 -0300

    readme

--- liquibase-project/changes/example-changelog-3.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset your.name:7 labels:example-label context:example-context
---comment: example comment
-create table car7 (
-    id int primary key not null,
-    model varchar(50) not null,
-)
---rollback DROP TABLE person;
