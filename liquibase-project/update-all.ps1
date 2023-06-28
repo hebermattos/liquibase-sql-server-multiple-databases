9308345ae41043ad1db980450629c58920976f10
hmattosaffinitiv <hmattos@affinitiv.com>
2023-06-28 12:13:21 -0300

    refactor

--- liquibase-project/update-all.ps1
+++ liquibase-project/update-all.ps1
@@ -1,6 +1,6 @@
 
 $configs = Get-ChildItem -Recurse ".\properties"  | Select-Object Name
-$command = "liquibase --defaultsFile=`"{CONFIG}`" update"
+$command = "liquibase --defaultsFile=`"properties\{CONFIG}`" update"
 
 foreach ($config in $configs) {
     $command = $command.Replace('{CONFIG}', $config.Name)
