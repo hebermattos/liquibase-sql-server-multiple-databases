4ed2f4aeaa58b945585327b34450747e456e03fb
hmattosaffinitiv <hmattos@affinitiv.com>
2023-06-28 12:29:18 -0300

    update verbose

--- liquibase-project/update-all.ps1
+++ liquibase-project/update-all.ps1
@@ -1,8 +1,9 @@
 
 $configs = Get-ChildItem -Recurse ".\properties"  | Select-Object Name
-$command = "liquibase --defaultsFile=`"properties\{CONFIG}`" update"
+$command = "liquibase --defaultsFile=`"properties\{CONFIG}`" update --show-summary=off"
 
 foreach ($config in $configs) {
     $command = $command.Replace('{CONFIG}', $config.Name)
+    #Write-Host $command
     Invoke-Expression $command
 }
